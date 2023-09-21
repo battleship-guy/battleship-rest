096bad8c4af232bb5c5c19d2c6526bca170a333c
Battleship Guy <lawoso@gmail.com>
2023-09-22 00:19:59 +0200

    Initialize Spring boot project

---
+++ src/main/java/nl/habiboellah/battleshiprest/BattleshipRestApplication.java
@@ -0,0 +1,13 @@
+package nl.habiboellah.battleshiprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/habiboellah/battleshiprest/BattleshipRestApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.habiboellah.battleshiprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
